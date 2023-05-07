8b91816a01ed792096c3ad602da157f62e021875
Ghazaaaaa <gymnastiarghaza@gmail.com>
2023-05-07 20:06:06 +0700

    Ghaza

--- BansosKu/Page/Pengaturan/Pengaturan.xaml.cs
+++ BansosKu/Page/Pengaturan/Pengaturan.xaml.cs
@@ -23,5 +23,10 @@ namespace BansosKu.Page.Pengaturan
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
