57e62b9121d42ae3270836aa81c4303df2220b2b
Iqius <iniiqius@gmail.com>
2023-06-13 10:52:31 +0700

    menambahkan clean code dan secure code pada Tracker1

--- BansosKu/Page/Tracking/Tracker1.xaml.cs
+++ BansosKu/Page/Tracking/Tracker1.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
 using APILibrary.API;
 using APILibrary.Model;
 using System.Text.RegularExpressions;
@@ -21,32 +20,50 @@ using BansosKu.Page.Home;
 
 namespace BansosKu.Page.Tracking
 {
-    /// <summary>
-    /// Interaction logic for Tracker1.xaml
-    /// </summary>
     public partial class Tracker1 : Window
     {
         private MyAPI _api = new MyAPI();
 
+        //Menampilkan daftar item TrxBansosModel pada lvBansos
         public Tracker1()
         {
             AppSettings.Default.id = 1;
             InitializeComponent();
             List<TrxBansosModel> listItems = _api.GetAllBansosUser<TrxBansosModel>(AppSettings.Default.id);
             lvBansos.ItemsSource = listItems;
-
         }
-
+        
+        //prosedur ntuk scroll pada Listview dengan nama lvBansos
         private void lvBansos_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
         {
 
         }
 
+        //prosedur untuk kembali ke page home dan menutup page saat ini
         private void back_Click(object sender, RoutedEventArgs e)
         {
             HomePage home = new HomePage();
             this.Close();
             home.Show();
         }
+
+        // Method untuk validasi input
+        private bool ValidateInput(string input)
+        {
+            string pattern = @"^[a-zA-Z0-9]+$";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        // Method untuk menghindari serangan SQL injection
+        private string SanitizeInput(string input)
+        {
+            return input.Replace("'", "''");
+        }
+
+        // Method untuk menghindari serangan cross-site scripting (XSS)
+        private string EscapeOutput(string output)
+        {
+            return System.Net.WebUtility.HtmlEncode(output);
+        }
     }
-    }
+}
